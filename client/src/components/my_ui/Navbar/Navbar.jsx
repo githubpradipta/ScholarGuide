875d4cd4d11dd1bdd72cf91432b69c6c07915af9
Pradipta Banerjee <banerjeepradipta08@gmail.com>
2024-07-27 16:35:44 +0530

    adding navbar and some detailing

--- client/src/components/my_ui/Navbar/Navbar.jsx
+++ client/src/components/my_ui/Navbar/Navbar.jsx
@@ -1,26 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
 import Button from '../Button/Button'
-import BrandLogo from '../../../assets/Logo/BrandLogo.png'
+import CollapsLogo from '../../../assets/Logo/CollapsLogo'
+import DownIcon from '../../../assets/Logo/DownIcon'
+import BrandIcon from '../../../assets/Logo/BrandIcon'
 
 export default function Navbar() {
+    const [collaps,setCollaps] = useState('uncollaps')
+    const collapsNav = ()=>{
+        console.log('clicked');
+        collaps=='uncollaps'? setCollaps('collaps'):setCollaps('uncollaps');
+    }
   return (
-    <div className="navbar">
-        <div className="brand">
-            <img src={BrandLogo} alt="Logo" />
-        </div>
-        <div className="links flex item-center">
+    <div className="navbar px-6 py-3 lg:px-12">
+        <Link to={'/home'} className="brand flex items-center justify-left">
+            <BrandIcon className="BrandIcon"/>
+            <p className="brandName text-white text-xl font-bold mx-1">Scholar<span className='text-[#fff643]'>Guide</span></p>
+        </Link>
+        <div className={`navContent ${collaps}`}>
+        <div className="links">
             <ul>
-                <Link to={'/home'}><li>Home</li></Link>
-                <Link><li>Notes</li></Link>
-                <Link><li>Upload</li></Link>
+                <li><Link to={'/home'}>Home</Link></li>
+                <li><Link>Notes</Link></li>
+                <li><Link>Upload</Link></li>
             </ul>
         </div>
         <div className="right">
             <Button varient={'outlined'}>Login</Button>
-            <Button varient={'filled'}>Quick Try</Button>
+            <Button varient={'filled'}>Sign up for full access</Button>
         </div>
+        </div>
+        
+        <div className="collapsIcon" onClick={collapsNav}>{collaps=='uncollaps'?<CollapsLogo/>:<DownIcon/>}</div>
+
     </div>
   )
 }
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/my_ui/Navbar/Navbar'
+import Button from '../../components/my_ui/Button/Button'
 export default function Home() {
   return (
     <>
     <Navbar/>
     <div className="head">
         <div className="head_box">
-          <p>Grow with Sharing</p>
-          
-          <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita, ab.</h1>
+          <p className='text-5xl'>Grow by Sharing</p>
+          <span className='text-7xl font-extrabold'>Notes</span>
+          <h1 className='mt-5 text-md px-10'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita, ab.</h1>
+          <div className="headBtn mt-7">
+            <Button className="py-4"varient={'filled'}>Explore</Button>
+            <Button varient={'outlined'}>Sign in</Button>
+          </div>
         </div>
       </div>
     </>
